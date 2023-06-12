08d2559fc3eed15f94f816e8149819b02ca353c1
sara-mohamd <saraamohamed0038@gmail.com>
2023-06-12 13:52:57 -0700

    test

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,11 @@
 
 int main(void)
 {
-char i = 'a', v = 'A';
+char i = 'a';
 
 for (i = 'a'; i <= 'z'; i++)
 {
-if (i == 'e' || i == 'q')
-exit;
-else
+if (i != 'e' || i != 'q')
 putchar(i);
 }
 putchar('\n');
